acc024b050f9616d44d6aef7411a7e58f9a24955
Oleg Zinchenko <1cdecoder@gmail.com>
2013-02-15 00:18:21 +0200

    a bit cleaned tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,22 +27,16 @@ define("CP_API_LOGIN_ID", "");
 define("CP_TRANSACTION_KEY", "");
 
 
-//define("AUTHORIZENET_LOG_FILE", dirname(__FILE__) . "/log");
+// define("AUTHORIZENET_LOG_FILE", dirname(__FILE__) . "/log");
 // Clear logfile
-//file_put_contents(AUTHORIZENET_LOG_FILE, "");
+// file_put_contents(AUTHORIZENET_LOG_FILE, "");
 
 if (!function_exists('curl_init')) {
     throw new Exception('AuthorizeNetSDK needs the CURL PHP extension.');
 }
-
-
 if (!function_exists('simplexml_load_file')) {
   throw new Exception('The AuthorizeNet SDK requires the SimpleXML PHP extension.');
 }
-
-//require_once dirname(dirname(__FILE__)) . '/AuthorizeNet.php';
-//require_once 'PHPUnit/Framework.php';
-
 if (AUTHORIZENET_API_LOGIN_ID == "") {
     die('Enter your merchant credentials in '.__FILE__.' before running the test suite.');
 }
